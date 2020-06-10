bc0a6910b2a315dbc390dc013fe2806d1a50d8c2
desmond107 <desmondchonte@gmail.com>
2020-06-10 14:24:42 +0300

    Roll up dice app using flutter

---
+++ dicee-flutter-master/lib/main.dart
@@ -0,0 +1,60 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        appBar: AppBar(
+          title: Text('Roll up dice'),
+          backgroundColor: Colors.teal,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int RightDiceNumbr = 1;
+
+  void change() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      RightDiceNumbr = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                change();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                change();
+              },
+              child: Image.asset('images/dice$RightDiceNumbr.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
